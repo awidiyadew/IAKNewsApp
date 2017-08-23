d3bb4cd0ca8e19024faca4c8564e4e2bb3d5af15
awidiyadew <awidiya.dewa@gmail.com>
2017-08-23 16:39:19 +0800

    setup actionbar with close button

--- app/src/main/java/id/iak/iaknewsapp/activity/DetailActivity.java
+++ app/src/main/java/id/iak/iaknewsapp/activity/DetailActivity.java
@@ -2,8 +2,12 @@ package id.iak.iaknewsapp.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
@@ -33,16 +37,43 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
 
-        if (getIntent().hasExtra(KEY_EXTRA_NEWS)){
-            String newsJson = getIntent().getStringExtra(KEY_EXTRA_NEWS);
-            mArticlesItem = new ArticlesItem().fromJson(newsJson);
-            Toast.makeText(this, "Show news " + mArticlesItem.getTitle(), Toast.LENGTH_SHORT).show();
-            setupWebView();
-            mWebView.loadUrl(mArticlesItem.getUrl());
-        } else {
+        /* jika activity di start tanpa membawa data news, maka finish activity*/
+        if (!getIntent().hasExtra(KEY_EXTRA_NEWS)){
+            Toast.makeText(this, "Data not found", Toast.LENGTH_SHORT).show();
             finish();
         }
 
+        String newsJson = getIntent().getStringExtra(KEY_EXTRA_NEWS);
+        mArticlesItem = new ArticlesItem().fromJson(newsJson);
+        Toast.makeText(this, "Show news " + mArticlesItem.getTitle(), Toast.LENGTH_SHORT).show();
+        setupWebView();
+        mWebView.loadUrl(mArticlesItem.getUrl());
+        setupActionBar();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return true;
+    }
+
+    private void setupActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle(R.string.actiobar_detail_title);
+            actionBar.setSubtitle(mArticlesItem.getUrl());
+
+            // mengaktifkan back button
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+            // setup close icon
+            Drawable backDrawable = ContextCompat.getDrawable(this, R.drawable.ic_action_close);
+            actionBar.setHomeAsUpIndicator(backDrawable);
+        }
     }
 
     private void setupWebView(){
